7782fdb7afb1f77359d1d10d5b08395aae4625f3
kasachander5 <145572316+kasachander5@users.noreply.github.com>
2023-09-26 17:02:00 +0800

    Update main.tf
    removed key

--- main.tf
+++ main.tf
@@ -5,8 +5,7 @@ provider "aws" {
 resource "aws_instance" "Demo" {
   ami           = "ami-0eeadc4ab092fef70"
   instance_type = "t2.micro"
-  key_name = "kasa"
-  tags = {
+    tags = {
     Name = "KASA Demo EC2"
   }
 }
